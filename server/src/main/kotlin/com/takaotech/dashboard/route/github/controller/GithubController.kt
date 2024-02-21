d80a592af6536ef6c342c7296a744978ef3c8fd7
samuele794 <samuelebruschi794@gmail.com>
2024-02-22 00:28:44 +0100

    Make getLanguagesByRepository mapping async

--- server/src/main/kotlin/com/takaotech/dashboard/route/github/controller/GithubController.kt
+++ server/src/main/kotlin/com/takaotech/dashboard/route/github/controller/GithubController.kt
@@ -1,16 +1,23 @@
 package com.takaotech.dashboard.route.github.controller
 
 import com.takaotech.dashboard.route.github.repository.GithubRepository
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
 import org.koin.core.annotation.Factory
 
 @Factory
 class GithubController(private val githubRepository: GithubRepository) {
 
-	suspend fun getStarsFromZeroAndStore() {
-		githubRepository.getAllStars().map {
-			it.copy(
-				languages = githubRepository.getLanguagesByRepository(it.id)
-			)
+	suspend fun getStarsFromZeroAndStore() = coroutineScope {
+		val mergedStarsWithLanguages = githubRepository.getAllStars().map {
+			async {
+				it.copy(
+					languages = githubRepository.getLanguagesByRepository(it.id)
+				)
+			}
 		}
+
+		mergedStarsWithLanguages.awaitAll()
 	}
 }
