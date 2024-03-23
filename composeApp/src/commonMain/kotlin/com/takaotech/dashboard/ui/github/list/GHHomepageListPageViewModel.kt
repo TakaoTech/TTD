fa4aef13fae8c12ab80775503eef291c70b1faa3
samuele794 <samuelebruschi794@gmail.com>
2024-03-23 11:37:36 +0100

    Make ghRepositoryList flow dispatch in IO

--- composeApp/src/commonMain/kotlin/com/takaotech/dashboard/ui/github/list/GHHomepageListPageViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/takaotech/dashboard/ui/github/list/GHHomepageListPageViewModel.kt
@@ -2,6 +2,9 @@ package com.takaotech.dashboard.ui.github.list
 
 import cafe.adriel.voyager.core.model.ScreenModel
 import com.takaotech.dashboard.repository.usecase.GHRepositoryListUseCase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
+import kotlinx.coroutines.flow.flowOn
 import org.koin.core.annotation.Factory
 
 @Factory
@@ -10,4 +13,5 @@ class GHHomepageListPageViewModel(
 ) : ScreenModel {
 
 	val repositoryList = ghRepositoryListUseCase.getGhRepositoryList()
+		.flowOn(Dispatchers.IO)
 }
