b67a2b5f9af1bbf662c2353e422cc0cf7e6646c1
samuele794 <samuelebruschi794@gmail.com>
2024-05-01 13:41:30 +0200

    WIP Github workflow integration

---
+++ .github/workflows/ttd_build_workflow.main.kts
@@ -0,0 +1,35 @@
+#!/usr/bin/env kotlin
+
+@file:DependsOn("io.github.typesafegithub:github-workflows-kt:1.15.0")
+
+import io.github.typesafegithub.workflows.actions.actions.CheckoutV4
+import io.github.typesafegithub.workflows.actions.actions.SetupJavaV4
+import io.github.typesafegithub.workflows.actions.gradle.ActionsSetupGradleV3
+import io.github.typesafegithub.workflows.domain.RunnerType.UbuntuLatest
+import io.github.typesafegithub.workflows.domain.triggers.Push
+import io.github.typesafegithub.workflows.dsl.workflow
+import io.github.typesafegithub.workflows.yaml.writeToFile
+
+workflow(
+	name = "Server build workflow",
+	on = listOf(Push()),
+	sourceFile = __FILE__.toPath(),
+) {
+	job(id = "build_server", runsOn = UbuntuLatest) {
+		uses(
+			name = "Setup Java",
+			action = SetupJavaV4(javaVersion = "17", distribution = SetupJavaV4.Distribution.Corretto)
+		)
+		uses(
+			name = "Setup Gradle",
+			action = ActionsSetupGradleV3()
+		)
+
+		uses(
+			name = "Checkout",
+			action = CheckoutV4()
+		)
+
+		run(name = "Generate image", command = "./gradlew server:publishImage")
+	}
+}.writeToFile()
