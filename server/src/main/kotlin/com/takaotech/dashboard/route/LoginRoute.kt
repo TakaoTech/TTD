b937bef1e6edef66b377067bac0d36897b3e53e5
samuele794 <samuelebruschi794@gmail.com>
2024-05-18 00:22:18 +0200

    Fixed test env + import

--- server/src/main/kotlin/com/takaotech/dashboard/route/LoginRoute.kt
+++ server/src/main/kotlin/com/takaotech/dashboard/route/LoginRoute.kt
@@ -1,10 +1,7 @@
 package com.takaotech.dashboard.route
 
-import com.takaotech.dashboard.plugins.auth.UserSession
 import io.ktor.server.application.*
-import io.ktor.server.auth.*
 import io.ktor.server.routing.*
-import io.ktor.server.sessions.*
 
 fun Application.loginRoute() {
 	routing {
