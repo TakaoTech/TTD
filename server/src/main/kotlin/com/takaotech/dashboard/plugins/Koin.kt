6382334313032caeaff6ee7d46956ced27772933
samuele794 <samuelebruschi794@gmail.com>
2024-06-15 12:31:36 +0200

    Order koin modules server

--- server/src/main/kotlin/com/takaotech/dashboard/plugins/Koin.kt
+++ server/src/main/kotlin/com/takaotech/dashboard/plugins/Koin.kt
@@ -1,9 +1,9 @@
 package com.takaotech.dashboard.plugins
 
 import com.takaotech.dashboard.configuration.CredentialConfig
-import com.takaotech.dashboard.di.getGeneralModule
 import com.takaotech.dashboard.configuration.DbConfiguration
 import com.takaotech.dashboard.configuration.GithubConfiguration
+import com.takaotech.dashboard.di.getGeneralModule
 import io.ktor.server.application.*
 import org.koin.ksp.generated.defaultModule
 import org.koin.ktor.plugin.Koin
@@ -16,6 +16,6 @@ fun Application.configureKoin(
 ) {
 	install(Koin) {
 		slf4jLogger()
-		modules(getGeneralModule(log, dbConfiguration, githubConfiguration, credentialConfig), defaultModule)
+		modules(defaultModule, getGeneralModule(log, dbConfiguration, githubConfiguration, credentialConfig))
 	}
 }
